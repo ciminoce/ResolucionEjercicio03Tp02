722aee1b3979456a2b3a65287a8a3a4a39711746
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-06-08 19:24:11 -0300

    Ejercicio completo

--- ResolucionEjercicio03Tp02/Program.cs
+++ ResolucionEjercicio03Tp02/Program.cs
@@ -25,7 +25,11 @@ namespace ResolucionEjercicio03Tp02
                                 Console.WriteLine($"La ecuación tiene una sola solución y es {raiz1}");
                             }else if (discriminante>0)
                             {
-                                
+                                HallarSoluciones(a, b, discriminante);
+                            }
+                            else
+                            {
+                                HallarSolucionesImaginarias(a, b, discriminante);
                             }
                         }
                         else
@@ -49,19 +53,28 @@ namespace ResolucionEjercicio03Tp02
                 Console.WriteLine("Coeficiente a mal ingresado");
             }
             
-            //Escribir "La ecuación tiene una sola solución y es ",RaizUnica;
-            //SiNo
-            //    Si Discriminante > 0 Entonces
-            //CalcularRaicesEcuacionCuadratica(a, b, Discriminante);
-            //SiNo
-            //CalcularRaicesImaginarias(a, b, Discriminante);
-            //FinSi
-            //    FinSi
-
-
-            //FinSi
 
 		}
+
+        private static void HallarSolucionesImaginarias(int nroA, int nroB, double discriminante)
+        {
+            double parteReal = -nroB / 2 * nroA;
+            double parteImaginaria = Math.Sqrt(Math.Abs(discriminante)) / 2 * nroA;
+            Console.WriteLine("La ecuación tiene 2 soluciones en el campo de los números imaginarios");
+            Console.WriteLine($"Solución 1={parteReal}+{parteImaginaria}i");
+            Console.WriteLine($"Solución 2={parteReal}-{parteImaginaria}i");
+
+        }
+
+        private static void HallarSoluciones( int nroA,  int nroB,  double discriminante)
+        {
+            double raiz1= (-nroB + Math.Sqrt(discriminante)) / 2 * nroA;
+            double raiz2 = (-nroB - Math.Sqrt(discriminante)) / 2 * nroA;
+            Console.WriteLine("La ecuación tiene 2 soluciones");
+            Console.WriteLine($"Solución 1={raiz1}");
+            Console.WriteLine($"Solución 2={raiz2}");
+
+        }
 
         private static double CalcularRaizUnica(int nroA,  int nroB)
         {
